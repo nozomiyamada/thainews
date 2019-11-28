bf30d9cf5afc754161a97e50b361fddf03c80cb7
nozomiyamada <y.nozomi320@gmail.com>
2019-11-28 14:56:45 +0700

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -1,4 +1,4 @@
-import os, json, glob, time, re, csv, shutil, html
+import os, json, glob, re, csv, shutil, html, itertools
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 import numpy as np
@@ -36,7 +36,7 @@ class NewsAnalyze:
 
     def tokenize(self):
         jsons = set(glob.glob(self.path + '*.json')) # all json files
-        tokenized_txt = {f.split('tokenized')[0]+'.json' for f in glob.glob(self.path + '*tokenized.tsv')} # already tokenized file
+        tokenized_txt = {f.split('tokenized')[0]+'.json' for f in glob.glob(self.path + '*tokenized.txt')} # already tokenized file
         to_be_tokenized = jsons - tokenized_txt # untokenized json files
         for json_path in to_be_tokenized:
             save_name = json_path.split('.json')[0] + 'tokenized.txt'
@@ -60,38 +60,40 @@ class NewsAnalyze:
                         if word not in stops:
                             stop[word] += 1
         # with stopwords
-        with open(self.path + 'wf.csv', 'w') as f:
-            wr = csv.writer(f, delimiter=',', lineterminator='\n')
-            wr.writerows(count.most_common())
+        df = pd.DataFrame(count.most_common(),columns=['word', 'count'],index=None)
+        df.to_csv(self.path + 'wf.csv',index=None)
         # without stopwords
-        with open(self.path + 'wf_stop.csv', 'w') as f:
-            wr = csv.writer(f, delimiter=',', lineterminator='\n')
-            wr.writerows(stop.most_common())
+        df = pd.DataFrame(stop.most_common(),columns=['word', 'count'],index=None)
+        df.to_csv(self.path + 'wf_stop.csv',index=None)
 
     def load_wf(self):
         """
         self.count, self.stop are pd.DataFrame
+            word count
             aaaa 2000
             bbbb 1000
         """
-        self.count = pd.read_csv(self.path + 'wf.csv', encoding='utf-8', header=None)
-        self.stop  = pd.read_csv(self.path + 'wf_stop.csv', encoding='utf-8', header=None)
+        self.count = pd.read_csv(self.path + 'wf.csv', encoding='utf-8')
+        self.stop  = pd.read_csv(self.path + 'wf_stop.csv', encoding='utf-8')
 
     def topn(self, n=50, delimiter=' '):
-        print(f'with stopwords\n{delimiter.join(self.count.iloc[:n,0])}\n')
-        print(f'w/o stopwords\n{delimiter.join(self.stop.iloc[:n,0])}')
+        self.load_wf()
+        print(f'with stopwords\n{delimiter.join(self.count["word"][:n])}\n')
+        print(f'w/o stopwords\n{delimiter.join(self.stop["word"][:n])}')
 
     def topn_th(self, n=50, delimiter=' '):
+        self.load_wf()
         only_th = [w for w in self.count.iloc[:3*n,0] if re.match(r'^[ก-๙][ก-๙ \.]*$', w)][:n]
         only_th2 = [w for w in self.stop.iloc[:3*n,0] if re.match(r'^[ก-๙][ก-๙ \.]*$', w)][:n]
         print(f'with stopwords\n{delimiter.join(only_th)}\n')
         print(f'w/o stopwords\n{delimiter.join(only_th2)}')
 
     def zipf(self):
+        self.load_wf()
         cx, sx = range(1, len(self.count)+1), range(1, len(self.stop)+1)
         plt.figure(figsize=(5,5))
-        plt.plot(cx, self.count.iloc[:,1], label='with stopwords')
-        plt.plot(sx, self.stop.iloc[:,1], label='w/o stopwords')
+        plt.plot(cx, self.count["count"], label='with stopwords')
+        plt.plot(sx, self.stop["count"], label='w/o stopwords')
         plt.title(f'word frequency : {self.publisher}')
         plt.ylabel('word frequency [log]')
         plt.xscale('log'); plt.yscale('log')
@@ -99,23 +101,25 @@ class NewsAnalyze:
         plt.xlim([1e0,1e7]), plt.ylim([1e0,1e7])
         plt.show()
 
-    def entropy(self):
+    def entropy(self, remove_punct=True):
         """
         entropy = -Σ p*log2(p) = -Σ c/N * log2(c/N) = -1/N Σ c*(log2(c) - log2(N))
         """
-        # with stopwords
-        N = sum(self.count.iloc[:,1])
-        V = len(self.count)
-        print(f'token: {N}\nvocab: {V}\nt/v: {N/V:.2f}')
-        logN = np.log2(N)
-        print(f'entropy with stop: {-1/N * sum(c * (np.log2(c)-logN) for c in self.count.iloc[:,1]):.2f}\n')
-
-        # without stopwords
-        N = sum(self.stop.iloc[:,1])
-        V = len(self.stop)
-        print(f'token: {N}\nvocab: {V}\nt/v: {N/V:.2f}')
-        logN = np.log2(N)
-        print(f'entropy w/o stop: {-1/N * sum(c * (np.log2(c)-logN) for c in self.stop.iloc[:,1]):.2f}')
+        self.load_wf()
+        # remove punctuations
+        count_removed = [c for i,w,c in self.count.itertuples() if re.match(r'^[A-zก-๙]', w)]
+        stop_removed = [c for i,w,c in self.stop.itertuples() if re.match(r'^[A-zก-๙]', w)]
+        count = self.count['count']
+        stop = self.stop['count']
+        
+        result = []
+        for i in [count, count_removed, stop, stop_removed]:
+            N, V = sum(i), len(i)
+            logN = np.log2(N)
+            entropy = round(-1/N * sum(c * (np.log2(c)-logN) for c in i), 2)
+            result.append([N, V, round(N/V,2), entropy])
+        return (pd.DataFrame(result, columns=['token','vocab','t/v','entropy'],
+        index=['with stop with punct:', 'with stop w/o  punct:', 'w/o  stop with punct:', 'w/o  stop w/o  punct:']))
 
 ### instantiation ###
 tr = NewsAnalyze('thairath')
@@ -124,7 +128,7 @@ dn = NewsAnalyze('dailynews')
 sn = NewsAnalyze('sanook')
 nhk = NewsAnalyze('nhk')
 
-def zipf_all(publishers=[tr,mc,dn,sn,nhk]):
+def zipf_all(publishers=[tr,dn,mc,sn,nhk]):
     fig = plt.figure()
     ax1 = fig.add_subplot(1, 2, 1)
     ax2 = fig.add_subplot(1, 2, 2)
@@ -145,3 +149,57 @@ def zipf_all(publishers=[tr,mc,dn,sn,nhk]):
     ax1.set_aspect('equal'); ax2.set_aspect('equal')
     fig.show()
 
+def freq_vec_sim(publishers=[tr,dn,mc,sn], nmax=10000, step=10, only_thai=True, plot=False, show_words=False):
+    """
+    calculate cosine similarity of each frequency vector (w/o stopwords)
+    :nmax: the size of vector
+    :only_thai: exclude non-thai character
+    :show_words: return word list 
+    """
+    # make a common word set
+    for i, p in enumerate(publishers):
+        p.load_wf()
+        if i == 0:
+            word_order = p.stop['word']
+            common_words = set(p.stop['word'])
+        else:
+            common_words &= set(p.stop['word'])
+
+    # make a ordered list of words
+    if only_thai:
+        word_order = [w for w in word_order if w in common_words if re.match(r'^[ก-๙][ก-๙ \.]*$', w)][:nmax]
+    else:
+        word_order = [w for w in word_order if w in common_words][:nmax]
+
+    # make an array of vectors
+    vecs = np.zeros((len(publishers), nmax), dtype=float)
+    for i, p in enumerate(publishers):
+        word_count_dic = dict(zip(p.stop['word'], p.stop['count']))
+        for j, w in enumerate(word_order):
+            vecs[i,j] = float(word_count_dic[w])
+
+    # calculate cosine similarity for each size
+    length = nmax // step
+    sim_array = np.zeros((len(publishers), len(publishers), length)) # tensor of rank 3
+    for i in range(len(publishers)):
+        for j in range(i+1, len(publishers)):
+            for n in range(length):
+                sim_array[i][j][n] = cossim(vecs[i][:step*(n+1)], vecs[j][:step*(n+1)])
+    pub_names = [p.publisher for p in publishers]
+    print(pd.DataFrame(sim_array[:,:,-1], index=pub_names,columns=pub_names))
+
+    # plot
+    if plot:
+        x = np.arange(step, nmax+1, step)
+        for i in range(len(publishers)):
+            for j in range(i+1, len(publishers)):
+                plt.plot(x, sim_array[i][j], label=f'{pub_names[i]}-{pub_names[j]}')
+        plt.xscale('log')
+        plt.legend(loc='best')
+        plt.title('cosine similarity of frequency vector')
+        plt.ylabel('size of vector')
+        plt.ylabel('cosine similarity')
+        plt.show()
+
+    if show_words:
+        return word_order
